55aa68ac6deb79f698d3dbf85f28ff5ae61dd166
kumatake-it-slow <kuma1.23g@gmail.com>
2019-05-02 19:46:04 +0900

    newアクションとcreateアクション

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,18 @@
 class PostsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
   end
 
-  def edit
+private
+
+  def post_params
+    params.require(:post).permit(:title,:body,:category)
   end
 
-  def update
-  end
-
-  def destroy
-  end
 end
