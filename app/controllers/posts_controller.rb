7292984904035f931577b64f543cd7a1f3ebaf91
kumatake-it-slow <kuma1.23g@gmail.com>
2019-05-03 09:11:02 +0900

    編集機能完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
     
   end
 
+
   def show
   @post = Post.find(params[:id])
   end
@@ -14,6 +15,16 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.save
+    redirect_to @post
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
     redirect_to @post
   end
 
