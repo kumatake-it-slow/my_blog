13d0fb37654d3749b9efb9d59c510903f4312fb2
kumatake-it-slow <kuma1.23g@gmail.com>
2019-05-03 10:43:54 +0900

    一覧機能完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,16 @@
 class PostsController < ApplicationController
 
   def index
-    
+    @posts = Post.all
+    @new_posts = Post.all
+    @author = Author.first
   end
 
 
   def show
-  @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
+
   def new
     @post = Post.new
   end
@@ -26,6 +29,12 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
 private
