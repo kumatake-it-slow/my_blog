dfdb33bd51462d7c1dde321227c21ec022b48bc6
kumatake-it-slow <kuma1.23g@gmail.com>
2019-05-03 11:34:13 +0900

    set

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -8,7 +9,6 @@ class PostsController < ApplicationController
 
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -22,17 +22,14 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to @post
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
@@ -41,6 +38,10 @@ private
 
   def post_params
     params.require(:post).permit(:title,:body,:category)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
